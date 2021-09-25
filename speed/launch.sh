6154f18b33963f2f9389c366c6e1bf22515f95dd
Tony Goacher <tgoacher@adjam.myzen.co.uk>
2021-09-25 16:29:44 +0100

    Removed log file pip from launch.sh

--- speed/launch.sh
+++ speed/launch.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-echo "Running " > /home/pi/tg.log
-sleep 10
-echo "Sleep over  " >> /home/pi/tg.log
+
 cd /home/pi/rpi_speed_video_camera/speed/
-/home/pi/rpi_speed_video_camera/speed/carspeed.py & >> /home/pi/tg.log
+/home/pi/rpi_speed_video_camera/speed/carspeed.py # > /home/pi/tg.log
 
